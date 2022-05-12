9c07e296681ac768600bd51507239ee71af0b07a
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2022-05-12 09:43:18 +0200

    Add Company to Application User

--- BulkyBook.Model/Models/ApplicationUser.cs
+++ BulkyBook.Model/Models/ApplicationUser.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BulkyBook.Model.Models
 {
@@ -16,5 +13,8 @@ namespace BulkyBook.Model.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId"),ValidateNever]
+        public Company Company { get; set; }
     }
 }
