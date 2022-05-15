f48a6b68f17d909c5c2e10752bb0a188c3015b58
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2022-05-15 09:07:12 +0200

    Adding Application User Repository

---
+++ BulkyBook.DataAccess/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using BulkyBook.Model.Models;
+
+namespace BulkyBook.DataAccess.IRepository
+{
+    public interface IApplicationUserRepository : IBaseRepository<ApplicationUser>
+    {
+        void Update(ApplicationUser applicationUser);
+    }
+}
---
+++ BulkyBook.DataAccess/Repository/ApplicationRepository.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBook.DataAccess.IRepository;
+using BulkyBook.Model.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ApplicationUserRepository : BaseRepository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ApplicationUserRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        void IApplicationUserRepository.Update(ApplicationUser applicationUser)
+        {
+            _context.ApplicationUsers.Update(applicationUser);
+        }
+    }
+}
