33868236d7a4061f6d4055e4d889660b322022ee
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2022-05-15 09:09:27 +0200

    Modifying Shopping Cart Model (Add User and ID)

--- BulkyBook.Model/ViewModels/ShoppingCart.cs
+++ BulkyBook.Model/ViewModels/ShoppingCart.cs
@@ -1,7 +1,9 @@
 ﻿using BulkyBook.Model.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,14 @@ namespace BulkyBook.Model.ViewModels
 {
     public class ShoppingCart
     {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId"),ValidateNever]
         public Product Product { get; set; }
         [Range(1,1000, ErrorMessage ="Please Enter A Value Between 1 And 1000!")]
         public int Count { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId"),ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
