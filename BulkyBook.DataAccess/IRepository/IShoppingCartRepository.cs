17112670e600d52c707158a9bda8a54b3da804e5
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2022-05-15 09:06:36 +0200

    Adding Shopping Cart Repository

---
+++ BulkyBook.DataAccess/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,9 @@
+﻿using BulkyBook.Model.ViewModels;
+
+namespace BulkyBook.DataAccess.IRepository
+{
+    public interface IShoppingCartRepository : IBaseRepository<ShoppingCart>
+    {
+        void Update(ShoppingCart shoppingCart);
+    }
+}
---
+++ BulkyBook.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBook.DataAccess.IRepository;
+using BulkyBook.Model.ViewModels;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ShoppingCartRepository : BaseRepository<ShoppingCart>, IShoppingCartRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ShoppingCartRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        void IShoppingCartRepository.Update(ShoppingCart shoppingCart)
+        {
+            _context.ShoppingCarts.Update(shoppingCart);
+        }
+    }
+}
