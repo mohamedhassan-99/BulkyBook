439b66100b001a87dadaf716d2997378ed479305
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2022-05-16 09:13:50 +0200

    Adding ShoppingCart Controller, View and Navbar Item

---
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using BulkyBook.DataAccess.IRepository;
+using BulkyBook.Model.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer"),Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var ShoppingCartVM = new ShoppingCartVM()
+            {
+                ListCart = _unitOfWork.ShoppingCart.GetAll(o => o.ApplicationUserId == claims.Value,"Product")
+            };
+            foreach(var cart in ShoppingCartVM.ListCart)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+                ShoppingCartVM.CartTotal += (cart.Price+ cart.Count);
+            }
+            return View(ShoppingCartVM);
+        }
+        private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
+        {
+            if(quantity <= 50)
+            {
+                return price;
+            }
+            else
+            {
+                if(quantity <= 100)
+                {
+                    return price50;
+                }
+
+                return price100;
+            }
+        }
+    }
+}
